2eafb8efc14f1d885709f04685fa955e50e8c0f2
MelvinWaiPhyoe <38678940+MelvinWaiPhyoe@users.noreply.github.com>
2020-07-28 16:38:34 +0630

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,89 @@
+/**
+ * iyi_robot အတွက် block design စမ်းသပ်ခြင်းဖြစ်ပါသည်။
+ */
+
+//% color="#FFFF00" weight=100 icon="\uf1ec" block="iyi test blocks"
+//% groups="['Line Follower', 'Ultrasonic', 'Servo', 'DCMotor']"
+namespace iyi_robot {
+    /**
+     * Line follower sensor 1ကို အသုံးပြုရန်။
+     */
+    //% block
+    //% group="Line Follower"
+    export function Sensor1() {
+
+    }
+
+    /**
+     * Line follower sensor 2ကို အသုံးပြုရန်။
+     */
+    //% block
+    //% group="Line Follower"
+    export function Sensor2() {
+
+    }
+
+    /**
+     * Line follower sensor 3ကို အသုံးပြုရန်။
+     */
+    //% block
+    //% group="Line Follower"
+    export function Sensor3() {
+
+    }
+
+    /**
+     * Line follower sensor 4ကို အသုံးပြုရန်။
+     */
+    //% block
+    //% group="Line Follower"
+    export function Sensor4() {
+
+    }
+
+    /**
+     * Line follower sensor 5ကို အသုံးပြုရန်။
+     */
+    //% block
+    //% group="Line Follower"
+    export function Sensor5() {
+
+    }  
+
+    /**
+     * Servo Motor အတွက် စမ်းသပ်ခြင်းဖြစ်ပါသည်။
+     */
+    //% block
+    //% group="Servo"
+    export function Servo() {
+
+    }    
+    
+    /**
+     * ဘယ်ဖက် DC Motor ကို မောင်းနှင်ရန်။
+     */
+    //% block
+    //% group="DC Motor"
+    export function left_motor() {
+
+    }
+
+    /**
+     * ညာဖက် DC Motor ကို မောင်းနှင်ရန်။
+     */
+    //% block
+    //% group=""DC Motor"
+    export function right_motor() {
+
+    }
+
+    /**
+     * Ultrasonic Sensorကို အသုံးပြုရန်။
+     */
+    //% block
+    //% group="Ultrasonic"
+    export function sonar() {
+
+    }
+
+}
